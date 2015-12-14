3f00532c598f77abdcdfcc45f4c67544e68bf24a
Hayden Parker <haydenkparker@gmail.com>
2015-12-13 19:38:42 -0800

    style for password pages

--- main.go
+++ main.go
@@ -10,15 +10,31 @@ func getting(c *gin.Context) {
 	c.HTML(http.StatusOK, "layout.tmpl", gin.H{})
 }
 
+func renderLogin(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.tmpl", gin.H{})
+}
+
+func render2FA(c *gin.Context) {
+	c.HTML(http.StatusOK, "2fa.tmpl", gin.H{})
+}
+
+func renderReset(c *gin.Context) {
+	c.HTML(http.StatusOK, "reset.tmpl", gin.H{})
+}
+
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(static.Serve("/vendor", static.LocalFile("vendor", false)))
+	router.Use(static.Serve("/", static.LocalFile("vendor", false)))
 	router.LoadHTMLGlob("views/*")
 	router.GET("/", getting)
+	router.GET("/login", renderLogin)
+	router.GET("/2fa", render2FA)
+	router.GET("/reset", renderReset)
 	return router
 }
 
 func main() {
-	//	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 	NewRouter().Run(":8080")
 }
