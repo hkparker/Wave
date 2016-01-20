fb21ff61f8e39260e240b8aab03c67156334b5b2
Hayden Parker <haydenkparker@gmail.com>
2016-01-19 23:44:17 -0800

    ginkgo structure

---
+++ controllers/collector_test.go
@@ -0,0 +1,13 @@
+package controllers_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+	//"net/http"
+	//"net/http/httptest"
+)
+
+var _ = Describe("Collector", func() {
+	It("", func() {
+	})
+})
