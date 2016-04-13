05706928b7bd4121674577ee769b119d79d6d605
Hayden Parker <haydenkparker@gmail.com>
2016-04-13 00:36:45 -0700

    random test emails

--- database/user_factory.go
+++ database/user_factory.go
@@ -1,23 +1,27 @@
 package database
 
 import (
+	"crypto/rand"
 	log "github.com/Sirupsen/logrus"
+	"github.com/jbenet/go-base58"
 )
 
 func TestUser(traits []string) (user User) {
+	email_bytes := make([]byte, 32)
+	rand.Read(email_bytes)
+	email := base58.Encode(email_bytes) + "@example.com"
 	log.WithFields(log.Fields{
 		"traits": traits,
 	}).Info("creating test user")
 	user = User{
 		Name:  "Turd Ferguson",
-		Email: "bighat@example.com",
+		Email: email,
 	}
 
 	for _, trait := range traits {
 		switch trait {
 		case "admin":
 			user.Name = "Wifi Jackson"
-			user.Email = "ughbluergna@murf.mnrgg"
 			user.Admin = true
 		}
 	}
