55ab772211fdd659f3bfaa3f4f7d47639dd3510b
Hayden <hayden@hkparker.com>
2016-11-04 22:05:07 -0700

    set tls test

--- models/tls_test.go
+++ models/tls_test.go
@@ -13,3 +13,22 @@ func TestDefaultTLSCertificateCanBeParsed(t *testing.T) {
 	_, err := tls.X509KeyPair(ca_data, key_data)
 	assert.Nil(err)
 }
+
+func TestSetTLSSetsTLS(t *testing.T) {
+	assert := assert.New(t)
+
+	original_cert, original_key := APITLSData()
+	new_cert, new_key := selfSignedCert()
+	assert.NotEqual(original_cert, new_cert)
+	assert.NotEqual(original_key, new_key)
+
+	err := SetTLS(map[string]string{
+		"ca_cert":     string(new_cert),
+		"private_key": string(new_key),
+	})
+	assert.Nil(err)
+
+	current_cert, current_key := APITLSData()
+	assert.Equal(current_cert, new_cert)
+	assert.Equal(current_key, new_key)
+}
