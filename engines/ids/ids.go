3d53287a77d9ee2dc8c1c961d8f1f1fbddb250b3
Hayden <hayden@hkparker.com>
2017-02-25 21:29:07 -0800

    move frame parsing code from collector

--- engines/ids/ids.go
+++ engines/ids/ids.go
@@ -1,7 +1,12 @@
 package ids
 
 import (
+	"encoding/json"
+	log "github.com/Sirupsen/logrus"
 	"github.com/hkparker/Wave/models"
 )
 
-func Insert(frame models.Wireless80211Frame) {}
+func Insert(frame models.Wireless80211Frame) {
+	str, _ := json.Marshal(frame)
+	log.Info(string(str))
+}
--- models/wiress80211frame.go
+++ models/wiress80211frame.go
@@ -1,8 +1,22 @@
 package models
 
 import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 )
+
+var ELEMENT_IDS = map[byte]string{
+	0:   "SSID",
+	1:   "SUPPORTED_RATES",
+	50:  "EXTENDED_SUPPORTED_RATES",
+	3:   "DS_PARAMETER_SET",
+	45:  "HT_CAPABILITIES",
+	127: "EXTENDED_CAPABILITIES",
+	221: "VENDOR_SPECIFIC",
+	//107: "??",
+	//191: "??",
+}
 
 type Wireless80211Frame struct {
 	Length           uint16
@@ -41,4 +55,65 @@ type Wireless80211Frame struct {
 	Checksum         uint32
 	Elements         map[string][]byte
 	Interface        string
+}
+
+func (frame *Wireless80211Frame) ParseElements(packet gopacket.Packet, ether *layers.Dot11) {
+	// since ether.Type is known, try to lookup the right one
+	//fmt.Println(ether.Type, uint8(ether.Type))
+	//fmt.Println(packet.Layers())
+	switch ether.Type {
+	case 0: //layers.LayerTypeDot11MgmtBeacon:
+		//fmt.Println("its a mgt")
+		//if beacon, ok := packet.Layer(layers.LayerTypeDot11MgmtBeacon).(*layers.Dot11MgmtBeacon); ok {
+		//	fmt.Println("got a beacon", beacon)
+		//	elements = ParseBeaconFrame()
+		//}
+	}
+	if probegreq, ok := packet.Layer(layers.LayerTypeDot11MgmtProbeReq).(*layers.Dot11MgmtProbeReq); ok {
+		//fmt.Println(ether.NextLayerType())
+		frame.Elements = ParseFrameElements(probegreq.LayerContents())
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11Data).(*layers.Dot11Data); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataCFAck).(*layers.Dot11DataCFAck); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataCFAckNoData).(*layers.Dot11DataCFAckNoData); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataCFAckPoll).(*layers.Dot11DataCFAckPoll); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataCFAckPollNoData).(*layers.Dot11DataCFAckPollNoData); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataCFPoll).(*layers.Dot11DataCFPoll); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataCFPollNoData).(*layers.Dot11DataCFPollNoData); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataNull).(*layers.Dot11DataNull); ok {
+		//} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOS).(*layers.Dot11DataQOS); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOSCFAckPollNoData).(*layers.Dot11DataQOSCFAckPollNoData); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOSCFPollNoData).(*layers.Dot11DataQOSCFPollNoData); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOSData).(*layers.Dot11DataQOSData); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOSDataCFAck).(*layers.Dot11DataQOSDataCFAck); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOSDataCFAckPoll).(*layers.Dot11DataQOSDataCFAckPoll); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOSDataCFPoll).(*layers.Dot11DataQOSDataCFPoll); ok {
+	} else if _, ok := packet.Layer(layers.LayerTypeDot11DataQOSNull).(*layers.Dot11DataQOSNull); ok {
+	}
+}
+
+func ParseFrameElements(stream []byte) (elements map[string][]byte) {
+	elements = map[string][]byte{}
+	for len(stream) > 0 {
+		field_id, remainder := stream[0], stream[1:]
+		stream = remainder
+
+		field, ok := ELEMENT_IDS[field_id]
+		if !ok {
+			log.WithFields(log.Fields{
+				"id": field_id,
+			}).Warn("unknown element id")
+		}
+
+		field_len, remainder := stream[0], stream[1:]
+		stream = remainder
+		if field_len == 0 {
+			continue
+		}
+
+		field_data, remainder := stream[:field_len], stream[field_len:]
+		stream = remainder
+
+		elements[field] = field_data
+	}
+	return
 }
